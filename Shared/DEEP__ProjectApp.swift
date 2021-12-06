fb849da89258cb65094cac9c1379a197881e6660
DEEP <srandeepkiran@gmail.com>
2021-12-06 12:24:37 -0500

    Initial Commit

---
+++ Shared/DEEP__ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEEP__ProjectApp.swift
+//  Shared
+//
+//  Created by kiran deep on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct DEEP__ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
